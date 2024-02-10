febff3df01129dd4df6802230fdea5a57b470254
Ali <ali.arioglu06@gmail.com>
2024-02-10 13:18:30 +0300

    insert layer

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -2,7 +2,7 @@
 
 import qs from "query-string";
 import { Search } from "lucide-react";
-import { useDebounce } from "@/hooks/useDebounce";
+import { useDebounce } from "@/hooks/use-debounce";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
